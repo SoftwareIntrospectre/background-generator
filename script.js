52965c231255e4add880ed1f4937f10d0e237e41
Tony Chacon <softwareintrospectre@gmail.com>
2018-10-04 07:19:26 -0400

    Changed variable names to colorA and colorB

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var css = document.querySelector("h3"); //grabs the only h3 tag on the page
-var color1 = document.querySelector(".color1"); //grabs css' "color1" class selector
-var color2 = document.querySelector(".color2"); //grabs css' "color2" class selector
+var colorA = document.querySelector(".color1"); //grabs css' "color1" class selector
+var colorB = document.querySelector(".color2"); //grabs css' "color2" class selector
 var body = document.getElementById("gradient"); //grabs html's background ID
 
 //checing to see if each variable works 
@@ -12,8 +12,8 @@ var body = document.getElementById("gradient"); //grabs html's background ID
 //changes background from color1's value to color2's value on a linear gradient (left to right)
 function setGradient(){
 	body.style.background = "linear-gradient(to right, " 
-	+ color1.value + ", "
-    + color2.value + ")";
+	+ colorA.value + ", "
+    + colorB.value + ")";
 
     css.textContent = body.style.background + ";";
 }
@@ -22,7 +22,7 @@ function setGradient(){
 // body.style.background  = "red";
 
 //detects when user's input changes on color1
-color1.addEventListener("input", setGradient);
+colorA.addEventListener("input", setGradient);
 
 //detects when user's input changes on color2
-color2.addEventListener("input", setGradient);
+colorB.addEventListener("input", setGradient);
